ef79c5802e60517932abe88780bea54b9f1ae316
Anish <giri.girianish@gmail.com>
2026-03-04 19:15:49 -0600

    further refined executer finder

--- airflow-core/src/airflow/jobs/scheduler_job_runner.py
+++ airflow-core/src/airflow/jobs/scheduler_job_runner.py
@@ -3221,7 +3221,12 @@ class SchedulerJobRunner(BaseJobRunner, LoggingMixin):
                 if (
                     executor.supports_connection_test
                     and executor.name
-                    and executor_name in (executor.name.alias, executor.name.module_path)
+                    and executor_name
+                    in (
+                        executor.name.alias,
+                        executor.name.module_path,
+                        executor.name.module_path.split(".")[-1],
+                    )
                 ):
                     return executor
             return None
