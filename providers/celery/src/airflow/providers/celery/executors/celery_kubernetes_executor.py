c655f8b95142efc6db2e4b2c6b21e2e3933ff671
Anish <giri.girianish@gmail.com>
2026-03-12 21:20:18 -0500

    fix import

--- providers/celery/src/airflow/providers/celery/executors/celery_kubernetes_executor.py
+++ providers/celery/src/airflow/providers/celery/executors/celery_kubernetes_executor.py
@@ -26,8 +26,8 @@ from deprecated import deprecated
 from airflow.exceptions import AirflowProviderDeprecationWarning
 from airflow.executors.base_executor import BaseExecutor
 from airflow.executors.workloads import WorkloadType
-from airflow.providers.celery.executors.celery_executor import AIRFLOW_V_3_0_PLUS, CeleryExecutor
-from airflow.providers.common.compat.sdk import conf
+from airflow.providers.celery.executors.celery_executor import CeleryExecutor  # noqa: TC001
+from airflow.providers.celery.version_compat import AIRFLOW_V_3_0_PLUS
 from airflow.utils.providers_configuration_loader import providers_configuration_loaded
 
 if TYPE_CHECKING:
