81422f9a08c7292b1ee4619bee7bee6d088c0932
Anish <giri.girianish@gmail.com>
2026-03-17 18:18:31 -0500

    version compat guard changes and undo unwated changes

--- providers/celery/tests/unit/celery/executors/test_celery_executor.py
+++ providers/celery/tests/unit/celery/executors/test_celery_executor.py
@@ -176,7 +176,7 @@ class TestCeleryExecutor:
     @mock.patch("airflow.providers.celery.executors.celery_executor.CeleryExecutor.sync")
     @mock.patch(
         "airflow.providers.celery.executors.celery_executor.CeleryExecutor."
-        + ("trigger_workloads" if AIRFLOW_V_3_2_PLUS else "trigger_tasks")
+        + ("trigger_workloads" if AIRFLOW_V_3_3_PLUS else "trigger_tasks")
     )
     @mock.patch("airflow.executors.base_executor.Stats.gauge")
     def test_gauge_executor_metrics(self, mock_stats_gauge, mock_trigger, mock_sync):
