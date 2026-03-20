0027f274d3b8b7b378463889c9e57483af3badd6
Anish <giri.girianish@gmail.com>
2026-03-20 12:34:54 -0500

    addressed review feedback

--- providers/celery/src/airflow/providers/celery/executors/celery_executor.py
+++ providers/celery/src/airflow/providers/celery/executors/celery_executor.py
@@ -39,6 +39,7 @@ from deprecated import deprecated
 
 from airflow.exceptions import AirflowProviderDeprecationWarning
 from airflow.executors.base_executor import BaseExecutor
+from airflow.executors.workloads.base import WorkloadType
 from airflow.providers.celery.executors import (
     celery_executor_utils as _celery_executor_utils,  # noqa: F401 # Needed to register Celery tasks at worker startup, see #63043.
 )
@@ -101,7 +102,9 @@ class CeleryExecutor(BaseExecutor):
     """
 
     supports_ad_hoc_ti_run: bool = True
-    supported_workload_types: frozenset[str] = frozenset({"ExecuteTask", "ExecuteCallback"})
+    supported_workload_types: frozenset[str] = frozenset(
+        {WorkloadType.EXECUTE_TASK, WorkloadType.EXECUTE_CALLBACK}
+    )
     sentry_integration: str = "sentry_sdk.integrations.celery.CeleryIntegration"
 
     # TODO: Remove this flag once providers depend on Airflow 3.2.
@@ -208,8 +211,8 @@ class CeleryExecutor(BaseExecutor):
                     )
                     self.workload_publish_retries[key] = retries + 1
                     continue
-            if key in self.queued_tasks:
-                self.queued_tasks.pop(key)
+            if key in self.executor_queues[WorkloadType.EXECUTE_TASK]:
+                self.executor_queues[WorkloadType.EXECUTE_TASK].pop(key)
             else:
                 self.executor_queues[WorkloadType.EXECUTE_CALLBACK].pop(key, None)
             self.workload_publish_retries.pop(key, None)
@@ -388,7 +391,7 @@ class CeleryExecutor(BaseExecutor):
             except Exception:
                 self.log.exception("Error revoking task instance %s from celery", ti.key)
         self.running.discard(ti.key)
-        self.queued_tasks.pop(ti.key, None)
+        self.executor_queues[WorkloadType.EXECUTE_TASK].pop(ti.key, None)
 
     @staticmethod
     def get_cli_commands() -> list[GroupCommand]:
