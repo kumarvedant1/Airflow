06ecbf3103e345111aaa3d65f74ea115ed4a5473
Miretpl <miretpl@gmail.com>
2026-04-05 15:13:03 +0200

    Refactor workers extraInitContainers-related tests

--- helm-tests/tests/helm_tests/airflow_aux/test_pod_template_file.py
+++ helm-tests/tests/helm_tests/airflow_aux/test_pod_template_file.py
@@ -1019,10 +1019,12 @@ class TestPodTemplateFile:
             chart_dir=self.temp_chart_dir,
         )
 
-        assert jmespath.search("spec.initContainers[-1]", docs[0]) == {
-            "name": "test-init-container",
-            "image": "test-registry/test-repo:test-tag",
-        }
+        assert jmespath.search("spec.initContainers", docs[0]) == [
+            {
+                "name": "test-init-container",
+                "image": "test-registry/test-repo:test-tag",
+            }
+        ]
 
     def test_should_template_extra_init_containers(self):
         docs = render_chart(
@@ -1035,9 +1037,11 @@ class TestPodTemplateFile:
             chart_dir=self.temp_chart_dir,
         )
 
-        assert jmespath.search("spec.initContainers[-1]", docs[0]) == {
-            "name": "release-name-test-init-container",
-        }
+        assert jmespath.search("spec.initContainers", docs[0]) == [
+            {
+                "name": "release-name-test-init-container",
+            }
+        ]
 
     def test_should_add_extra_containers(self):
         docs = render_chart(
