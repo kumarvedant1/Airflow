1f560ff80596048491cc6fbc90043354e93ca523
Miretpl <miretpl@gmail.com>
2026-04-05 20:40:57 +0200

    Add workers.celery.extraVolumes & workers.kubernetes.extraVolumes

--- helm-tests/tests/helm_tests/airflow_aux/test_pod_template_file.py
+++ helm-tests/tests/helm_tests/airflow_aux/test_pod_template_file.py
@@ -863,13 +863,20 @@ class TestPodTemplateFile:
 
         assert jmespath.search("spec.securityContext.runAsUser", docs[0]) == 1
 
-    def test_should_create_valid_volume(self):
-        docs = render_chart(
-            values={
-                "workers": {
-                    "extraVolumes": [{"name": "test-volume-{{ .Chart.Name }}", "emptyDir": {}}],
-                }
+    @pytest.mark.parametrize(
+        "workers_values",
+        [
+            {"extraVolumes": [{"name": "test-volume-{{ .Chart.Name }}", "emptyDir": {}}]},
+            {"kubernetes": {"extraVolumes": [{"name": "test-volume-{{ .Chart.Name }}", "emptyDir": {}}]}},
+            {
+                "extraVolumes": [{"name": "test", "emptyDir": {}}],
+                "kubernetes": {"extraVolumes": [{"name": "test-volume-{{ .Chart.Name }}", "emptyDir": {}}]},
             },
+        ],
+    )
+    def test_should_create_valid_volume(self, workers_values):
+        docs = render_chart(
+            values={"workers": workers_values},
             show_only=["templates/pod-template-file.yaml"],
             chart_dir=self.temp_chart_dir,
         )
